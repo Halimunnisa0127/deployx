c41fcc50be6f70d13d0d9d146cf4f205ac4fd92b
Munni <shaikmunni1314@gmail.com>
2026-08-21 15:43:01 +0530

    "fixing cookies"

--- backend/src/utils/helpers/cookie.helper.js
+++ backend/src/utils/helpers/cookie.helper.js
@@ -1,7 +1,28 @@
 const config = require('../../config/env/env');
 const { COOKIE_NAMES } = require('../../shared/constants/constants');
 
+const clearLegacyCookies = (res) => {
+  const legacyPaths = [
+    '/integrations/github/oauth/callback',
+    '/integrations/google/oauth/callback',
+    '/auth/refresh-token',
+    '/auth/login',
+    '/auth/register',
+  ];
+
+  legacyPaths.forEach((path) => {
+    res.cookie(COOKIE_NAMES.REFRESH_TOKEN, '', {
+      httpOnly: true,
+      secure: config.isProduction,
+      sameSite: config.isProduction ? 'none' : 'strict',
+      expires: new Date(0),
+      path,
+    });
+  });
+};
+
 const setRefreshTokenCookie = (res, token) => {
+  clearLegacyCookies(res);
   res.cookie(COOKIE_NAMES.REFRESH_TOKEN, token, {
     httpOnly: true,
     secure: config.isProduction,
@@ -12,6 +33,7 @@ const setRefreshTokenCookie = (res, token) => {
 };
 
 const clearRefreshTokenCookie = (res) => {
+  clearLegacyCookies(res);
   res.cookie(COOKIE_NAMES.REFRESH_TOKEN, '', {
     httpOnly: true,
     secure: config.isProduction,
